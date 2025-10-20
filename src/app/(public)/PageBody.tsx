49f5e53c3b10f0f7f3eb91c223d51a61658f7826
devops-anz <fayazahsanul@gmail.com>
2025-10-20 18:10:39 +0600

    rebuild

--- src/app/(public)/PageBody.tsx
+++ src/app/(public)/PageBody.tsx
@@ -1,15 +1,15 @@
 'use client';
 import { Fragment } from 'react';
-// import About from 'view/page/home/about';
-// import Contact from 'view/page/home/contact';
-// import FeaturedProjects from 'view/page/home/featuredProjects';
+import About from 'view/page/home/about';
+import Contact from 'view/page/home/contact';
+import FeaturedProjects from 'view/page/home/featuredProjects';
 import Hero from 'view/page/home/hero';
-// import Quote from 'view/page/home/quote';
+import Quote from 'view/page/home/quote';
 // import RecentWorks from 'view/page/home/recentWorks';
-// import Services from 'view/page/home/service';
-// import Testimonials from 'view/page/home/testimonials';
-// import Timeline from 'view/page/home/timeline';
-// import TopServices from 'view/page/home/topServices';
+import Services from 'view/page/home/service';
+import Testimonials from 'view/page/home/testimonials';
+import Timeline from 'view/page/home/timeline';
+import TopServices from 'view/page/home/topServices';
 import ScrollTop from 'view/ui/shared-component/scrollTop';
 // import Blog from 'view/page/home/blogs';
 
@@ -27,14 +27,14 @@ interface PageBodyProps {
 
 const PageBody = ({
   heroData,
-  // serviceData,
-  // aboutData,
-  // topServicesData,
-  // timelineData,
-  // quoteData,
-  // featuredProjectsData,
-  // testimonialsData,
-  // contactData
+  serviceData,
+  aboutData,
+  topServicesData,
+  timelineData,
+  quoteData,
+  featuredProjectsData,
+  testimonialsData,
+  contactData
 }: PageBodyProps) => {
 
   return (
@@ -43,11 +43,11 @@ const PageBody = ({
         heroData?.data?.is_active &&
         <Hero heroData={heroData?.data} />
       }
-      {/* {
-        false &&
+      {
+        serviceData?.data?.is_active &&
         <Services serviceData={serviceData?.data?.services} />
-      } */}
-      {/* {
+      }
+      {
         aboutData?.data?.is_active &&
         <About aboutData={aboutData?.data} />
       }
@@ -74,7 +74,7 @@ const PageBody = ({
       {
         contactData?.data?.is_active &&
         <Contact contactData={contactData?.data} />
-      } */}
+      }
       <ScrollTop />
     </Fragment>
   );
